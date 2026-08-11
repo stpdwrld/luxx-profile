7f53916b502dc85249b487fe33ad699fd22c7fe2
StupidX2 <luckyzp77@gmail.com>
2026-08-11 18:43:18 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,26 @@ export default {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <title>LUXX</title>
 
+<!-- FAVICON -->
+<link
+  rel="icon"
+  type="image/png"
+  href="https://raw.githubusercontent.com/stpdwrld/bahanprof/main/logo.png"
+>
+
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-<link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400&family=Inter:wght@300;400;500&family=Playfair+Display:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet">
+<link
+  href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400&family=Inter:wght@300;400;500&family=Playfair+Display:ital,wght@0,400;0,500;1,400&display=swap"
+  rel="stylesheet"
+>
 
 <style>
 
